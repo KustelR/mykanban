fcc3e45a9b97045ce573111c6eaa1348b78026c2
Kustel <dmitriysstudios2@gmail.com>
2025-02-13 06:18:58 +0400

    moved auth to separate page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import AuthForm from "@/ui/AuthForm";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid place-center md:grid-cols-6">
-      <AuthForm className="h-full w-full mx-2 my-2 md:mx-0 md:col-start-3 md:col-span-2"></AuthForm>
-    </div>
-  );
+  return <div></div>;
 }
