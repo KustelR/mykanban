61ac502aa6b79e4b6818139e12a08f706f363177
Kustel <dmitriysstudios2@gmail.com>
2025-02-13 09:24:15 +0400

    page for projects

---
+++ src/app/[project_id]/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/ui/Card";
+import CardColumn from "@/ui/CardColumn";
+import Kanban from "@/ui/Kanban";
+
+export default function Project() {
+  return (
+    <div className="">
+      <Kanban label="TEST" className="w-[1000px] h-[500px]"></Kanban>
+    </div>
+  );
+}
