8f656dc883579979881b3cbb524bb6b764aa0143
Kustel <dmitriysstudios2@gmail.com>
2025-02-13 09:25:01 +0400

    types for cards and columns

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,13 @@
 declare global {
   type validator = (input: string) => [boolean, string];
+  type CardData = {
+    title: string;
+    description: string;
+    tags: Array<string>;
+  };
+  type ColData = {
+    header: string;
+    cards: Array<CardData>;
+  };
 }
 export {};
