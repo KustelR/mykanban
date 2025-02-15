b3dbdf9b3c832ecbba3577be2bea34fdcff2d1d5
KustelR <dmitriysstudios2@gmail.com>
2025-02-16 00:58:15 +0400

    more responsive

--- src/app/[project_id]/page.tsx
+++ src/app/[project_id]/page.tsx
@@ -5,7 +5,7 @@ import Kanban from "@/ui/Kanban";
 export default function Project() {
   return (
     <div className="">
-      <Kanban label="TEST" className="w-[1000px] h-[500px]"></Kanban>
+      <Kanban label="TEST" className="w-full md:max-w-fit"></Kanban>
     </div>
   );
 }
