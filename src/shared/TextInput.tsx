38a77f2a4e6f98f2548d514c3c6601dcf125a2df
KustelR <dmitriysstudios2@gmail.com>
2025-02-15 22:29:59 +0400

    text area option

--- src/shared/TextInput.tsx
+++ src/shared/TextInput.tsx
@@ -1,19 +1,34 @@
 "use client";
 
 import { ReactNode, useState } from "react";
-import ShowIf from "./ShowIf";
+import React from "react";
 
 type TextInputProps = {
+  className?: string;
   id: string;
   label: ReactNode;
+  defaultValue?: string;
   placeholder?: string;
   type?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void;
   validators?: Array<validator>;
+  area?: boolean;
 };
 
 export default function TextInput(props: TextInputProps) {
-  const { id, label, placeholder, validators, type, onChange } = props;
+  const {
+    id,
+    label,
+    placeholder,
+    validators,
+    type,
+    defaultValue,
+    onChange,
+    area,
+    className,
+  } = props;
   const [isFocused, setIsFocused] = useState(false);
   const [isValid, setIsValid] = useState(false);
   const defaultValidationMessages: Array<string> = [];
@@ -21,8 +36,9 @@ export default function TextInput(props: TextInputProps) {
     defaultValidationMessages,
   );
   const [data, setData] = useState("");
+
   return (
-    <div className="lg:text-3xl">
+    <div className={className}>
       <label htmlFor={id}>{label}</label>
 
       <div className="relative">
@@ -31,48 +47,82 @@ export default function TextInput(props: TextInputProps) {
         >
           {placeholder}
         </span>
-        <input
-          className={`${isValid ? "dark:border-green-700" : ""} invalid:border-red-600 w-full px-2 rounded-md border-[1px] focus:outline-none dark:border-neutral-700 dark:focus:bg-neutral-700 dark:bg-neutral-800 `}
-          type={type ? type : "text"}
-          id={id}
-          onChange={(e) => {
-            setData(e.target.value);
-            if (validators) {
-              const newValidMsgs = validate(e.target, validators);
+        {!area && (
+          <input
+            className={`${isValid ? "dark:border-green-700" : ""} invalid:border-red-600 w-full px-2 rounded-md border-[1px] focus:outline-none dark:border-neutral-700 dark:focus:bg-neutral-700 dark:bg-neutral-800 `}
+            type={type ? type : "text"}
+            id={id}
+            placeholder={defaultValue}
+            onChange={(e) => {
+              validateInput(
+                e.target,
+                setData,
+                setIsValid,
+                setValidationMessages,
+                validators,
+              );
+              if (onChange && isValid) onChange(e);
+            }}
+            onFocus={() => {
+              setIsFocused(!isFocused);
+            }}
+            onBlur={(e) => {
+              setIsFocused(!isFocused);
+              if (validators) {
+                const newValidMsgs = validate(e.target, validators);
 
-              if (newValidMsgs.length === 0) {
-                setIsValid(true);
-              } else {
-                setIsValid(false);
+                if (newValidMsgs.length === 0) {
+                  setIsValid(true);
+                } else {
+                  setIsValid(false);
+                }
+                setValidationMessages(newValidMsgs);
               }
-              setValidationMessages(newValidMsgs);
-            }
-            if (onChange && isValid) {
-              onChange(e);
-            }
-          }}
-          onFocus={() => {
-            setIsFocused(!isFocused);
-          }}
-          onBlur={(e) => {
-            setIsFocused(!isFocused);
-            if (validators) {
-              const newValidMsgs = validate(e.target, validators);
+            }}
+            onInvalid={(e) => {
+              e.preventDefault();
+            }}
+          />
+        )}
+        {area && (
+          <textarea
+            className={`${isValid ? "dark:border-green-700" : ""} invalid:border-red-600 w-full px-2 rounded-md border-[1px] focus:outline-none dark:border-neutral-700 dark:focus:bg-neutral-700 dark:bg-neutral-800 `}
+            id={id}
+            placeholder={defaultValue}
+            onChange={(e) => {
+              validateInput(
+                e.target,
+                setData,
+                setIsValid,
+                setValidationMessages,
+              );
+              if (onChange && isValid) {
+                onChange(e);
+              }
+            }}
+            onFocus={() => {
+              setIsFocused(!isFocused);
+            }}
+            onBlur={(e) => {
+              setIsFocused(!isFocused);
+              if (validators) {
+                const newValidMsgs = validate(e.target, validators);
 
-              if (newValidMsgs.length === 0) {
-                setIsValid(true);
-              } else {
-                setIsValid(false);
+                if (newValidMsgs.length === 0) {
+                  setIsValid(true);
+                } else {
+                  setIsValid(false);
+                }
+                setValidationMessages(newValidMsgs);
               }
-              setValidationMessages(newValidMsgs);
-            }
-          }}
-          onInvalid={(e) => {
-            e.preventDefault();
-          }}
-        />
+            }}
+            onInvalid={(e) => {
+              e.preventDefault();
+            }}
+          />
+        )}
       </div>
-      <ShowIf isVisible={validationMessages.length > 0}>
+      {validationMessages.length > 0 && (
         <ul className="text-sm/6">
           {validationMessages.map((message, idx) => {
             return (
@@ -84,13 +134,13 @@ export default function TextInput(props: TextInputProps) {
             );
           })}
         </ul>
-      </ShowIf>
+      )}
     </div>
   );
 }
 
 function validate(
-  target: EventTarget & HTMLInputElement,
+  target: HTMLInputElement | HTMLTextAreaElement,
   validators: Array<validator>,
 ): Array<string> {
   let newValidMsgs: Array<string> = [];
@@ -105,4 +155,25 @@ function validate(
     target.reportValidity();
   });
   return newValidMsgs;
+}
+function validateInput(
+  target: HTMLInputElement | HTMLTextAreaElement,
+  setData: (arg: string) => void,
+  setIsValid: (arg: boolean) => void,
+  setValidationMessages: (arg: string[]) => void,
+  validators?: Array<validator>,
+): void {
+  let isValid: boolean = false;
+  if (validators && validators.length > 0) {
+    const newValidMsgs = validate(target, validators);
+
+    if (newValidMsgs.length === 0) {
+      isValid = true;
+    }
+    setValidationMessages(newValidMsgs);
+  } else {
+    isValid = true;
+  }
+  setIsValid(isValid);
+  setData(target.value);
 }
