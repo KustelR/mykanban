9e59a65f39e9f411a9a8ecf05a0e66cd21825d3d
KustelR <dmitriysstudios2@gmail.com>
2025-02-16 15:42:10 +0400

    custom class names

--- src/shared/TextButton.tsx
+++ src/shared/TextButton.tsx
@@ -3,14 +3,15 @@ import { MouseEvent as ReactMouseEvent, ReactNode } from "react";
 type ButtonProps = {
   children: string;
   onClick?: (event: ReactMouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  className?: string;
 };
 
 export default function TextButton(props: ButtonProps) {
-  const { children, onClick } = props;
+  const { children, onClick, className } = props;
   return (
     <button
       onClick={onClick}
-      className="border-[1px] px-4 py-2 rounded-xl dark:border-neutral-700 dark:hover:bg-neutral-800"
+      className={`border-[1px] px-4 py-2 rounded-xl bg-neutral-200 hover:bg-slate-300 dark:bg-neutral-900 dark:border-neutral-700 dark:hover:bg-neutral-800 ${className}`}
     >
       {children}
     </button>
