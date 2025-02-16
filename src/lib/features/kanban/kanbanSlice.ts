a141aff217433d884c2f4aaf70da6bc499d46516
KustelR <dmitriysstudios2@gmail.com>
2025-02-16 15:41:43 +0400

    added kanban setter

--- src/lib/features/kanban/kanbanSlice.ts
+++ src/lib/features/kanban/kanbanSlice.ts
@@ -1,9 +1,4 @@
-import {
-  createAction,
-  createReducer,
-  createSlice,
-  nanoid,
-} from "@reduxjs/toolkit";
+import { createAction, createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState: KanbanState = {
   columns: [],
@@ -11,14 +6,22 @@ const initialState: KanbanState = {
 };
 
 const addColumnAction = createAction<ColData>("kanban/addColumn");
+const setKanbanAction = createAction<KanbanState>("kanban/setKanban");
 const addCardByColIdAction = createAction<[string, CardData]>(
   "kanban/addCardByColId",
 );
+const replaceCardAction =
+  createAction<[string, string, CardData]>("kanban/replaceCard");
 
 export const kanbanSlice = createSlice({
   name: "kanban",
   initialState,
   reducers: {
+    setKanban: (state, action) => {
+      const payload = action.payload as KanbanState;
+      state.columns = payload.columns;
+      state.label = payload.label;
+    },
     addColumn: (state, action) => {
       const payload = action.payload as ColData;
       const id = nanoid();
@@ -39,10 +42,34 @@ export const kanbanSlice = createSlice({
       const targetCol = { ...state.columns[targetColIdx] };
       if (!targetCol) return;
       targetCol.cards.push(payload[1]);
-      state.columns.splice(targetColIdx, 1, targetCol);
+
+      const stateClone = { ...state };
+      stateClone.columns.splice(targetColIdx, 1, targetCol);
+      state = stateClone;
+    },
+    replaceCard: (state, action) => {
+      const payload = action.payload as [string, string, CardData];
+      const targetColIdx = state.columns.findIndex((column) => {
+        return column.id === payload[0];
+      });
+      const targetCol = { ...state.columns[targetColIdx] };
+      if (!targetCol) return;
+      const targetCardIdx = targetCol.cards.findIndex((card) => {
+        return card.id === payload[1];
+      });
+      targetCol.cards.splice(targetCardIdx, 1, payload[2]);
+
+      const stateClone = { ...state };
+      stateClone.columns.splice(targetColIdx, 1, targetCol);
+      state = stateClone;
     },
   },
 });
 
-export { addColumnAction, addCardByColIdAction };
+export {
+  addColumnAction,
+  addCardByColIdAction,
+  replaceCardAction,
+  setKanbanAction,
+};
 export default kanbanSlice.reducer;
