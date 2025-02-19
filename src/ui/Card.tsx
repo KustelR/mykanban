6a721cb8172047f590e2470c2b4b2d87a899c570
KustelR <dmitriysstudios2@gmail.com>
2025-02-19 23:14:28 +0400

    added card moving top and down in the list

--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import CardEditor, { CardEditorPortal } from "./CardEditor";
 import ChangeIcon from "@public/change.svg";
+import ArrowUpIcon from "@public/arrow-up.svg";
+import ArrowDownIcon from "@public/arrow-down.svg";
 import DeleteIcon from "@public/delete.svg";
 import { useDrag } from "react-dnd";
 import { ItemTypes } from "@/Constants";
-import { removeCard } from "@/scripts/kanban";
+import { moveCard, removeCard } from "@/scripts/kanban";
+import Tag from "@/ui/Tag";
+import TagList from "./TagList";
 
 type CardProps = {
   cardData: CardData;
   blocked?: boolean;
   cards?: Array<CardData>;
-  colId?: string;
   setCards?: (arg: Array<CardData>) => void;
   setColumns: (arg: Array<ColData>) => void;
   columns: Array<ColData>;
@@ -41,48 +44,74 @@ export function Card(props: CardProps) {
         setIsActive(false);
       }}
     >
-      <ul
-        className={`${isActive ? "" : "hidden"} absolute flex flex-row right-0 top-0 h-fit [font-size:0]`}
-      >
-        <li>
-          <button
-            className="h-5 w-5 flex flex-wrap content-center justify-center bg-green-600/50 hover:bg-green-700/50"
-            onClick={() => {
-              if (!blocked) setIsRedacting(true);
-            }}
-          >
-            <ChangeIcon
-              width={16}
-              height={16}
-              className="*:stroke-white"
-            ></ChangeIcon>
-          </button>
-        </li>
-        <li>
-          <button
-            className="h-5 w-5 flex flex-wrap content-center justify-center bg-red-600/50 hover:bg-red-700/50"
-            onClick={(e) => {
-              if (cards && setCards) {
-                setColumns(removeCard(cardData.id, cardData.colId, columns));
-              }
-            }}
-          >
-            <DeleteIcon
-              width={16}
-              height={16}
-              className=" *:stroke-white"
-            ></DeleteIcon>
-          </button>
-        </li>
-      </ul>
+      {!blocked && (
+        <ul
+          className={`${isActive ? "" : "hidden"} absolute flex flex-row right-0 top-0 h-fit [font-size:0]`}
+        >
+          <li>
+            <button
+              className="h-5 w-5 flex flex-wrap content-center justify-center bg-blue-800 hover:bg-blue-900"
+              onClick={() => {
+                setColumns(moveCard(columns, cardData.colId, cardData.id, 1));
+              }}
+            >
+              <ArrowUpIcon
+                width={16}
+                height={16}
+                className="*:stroke-white *:fill-transparent"
+              ></ArrowUpIcon>
+            </button>
+          </li>
+          <li>
+            <button
+              className="h-5 w-5 flex flex-wrap content-center justify-center bg-blue-800 hover:bg-blue-900"
+              onClick={() => {
+                setColumns(moveCard(columns, cardData.colId, cardData.id, -1));
+              }}
+            >
+              <ArrowDownIcon
+                width={16}
+                height={16}
+                className="*:stroke-white *:fill-transparent"
+              ></ArrowDownIcon>
+            </button>
+          </li>
+          <li>
+            <button
+              className="h-5 w-5 flex flex-wrap content-center justify-center bg-green-800 hover:bg-green-900"
+              onClick={() => {
+                if (!blocked) setIsRedacting(true);
+              }}
+            >
+              <ChangeIcon
+                width={16}
+                height={16}
+                className="*:stroke-white"
+              ></ChangeIcon>
+            </button>
+          </li>
+          <li>
+            <button
+              className="h-5 w-5 flex flex-wrap content-center justify-center bg-red-600/50 hover:bg-red-700/50"
+              onClick={(e) => {
+                if (cards && setCards) {
+                  setColumns(removeCard(cardData.id, cardData.colId, columns));
+                }
+              }}
+            >
+              <DeleteIcon
+                width={16}
+                height={16}
+                className=" *:stroke-white"
+              ></DeleteIcon>
+            </button>
+          </li>
+        </ul>
+      )}
       <section className="rounded-md hover:bg-neutral-300 border-[1px] dark:border-neutral-700 hover:dark:bg-neutral-800 p-2 ">
         <header className="font-bold">{title}</header>
         <p className="text-wrap break-words line-clamp-3">{description}</p>
-        <ul className="flex flex-wrap *:rounded-md *:bg-cyan-700/20 *:mr-1 *:mb-1 *:px-1 *:border-[1px] *:border-cyan-600/30">
-          {tags?.map((tag, idx) => {
-            return <li key={idx}>{tag}</li>;
-          })}
-        </ul>
+        <TagList tags={tags} />
       </section>
       {isRedacting && (
         <CardEditorPortal
