9d9109bc19ee0025734ba0ea57df83656b2eb391
KustelR <dmitriysstudios2@gmail.com>
2025-03-01 03:18:07 +0400

    draw order support

--- src/scripts/kanban.ts
+++ src/scripts/kanban.ts
@@ -15,12 +15,18 @@ export function pushNewCard(
     return colId === col.id;
   });
   if (changingColumnIdx === -1) throw new Error("No column was found");
-  const newColumnCards = [...newColumns[changingColumnIdx].cards];
-  const newCard = { ...card, ...{ colId: colId } };
+  let cards = newColumns[changingColumnIdx].cards;
+  if (!cards) cards = [];
+  const newColumnCards = [...cards];
+  const newCard: CardData = {
+    ...card,
+    columnId: colId,
+    order: cards.length + 1,
+  };
   newColumnCards.push(newCard);
   newColumns[changingColumnIdx] = {
     ...newColumns[changingColumnIdx],
-    ...{ cards: newColumnCards },
+    cards: newColumnCards,
   };
   return newColumns;
 }
@@ -39,12 +45,14 @@ export function removeCard(
   const colIdx = columns.findIndex((col) => {
     return col.id === colId;
   });
+
   if (colIdx === -1) throw new Error("No column was found");
-  const card: CardData | undefined = columns[colIdx].cards.find((card) => {
+  let cards = columns[colIdx].cards;
+  if (!cards) return columns;
+  const card: CardData | undefined = cards.find((card) => {
     return card.id === cardId;
   });
   if (!card) throw new Error("No card was found");
-  let cards = [...columns[colIdx].cards];
   cards = cards.map((c) => {
     if (c.order < card.order) return c;
     return { ...c, order: c.order - 1 };
@@ -65,12 +73,12 @@ export function addColumn(
   options?: { place?: "start" | "end" },
 ): Array<ColData> {
   if (!options) {
-    return [...columns, column];
+    return [...columns, { ...column, order: columns.length + 1 }];
   } else {
     if (options.place === "start") {
       return [column, ...columns];
     } else if (options.place === "end") {
-      return [...columns, column];
+      return [...columns, { ...column, order: columns.length + 1 }];
     } else {
       return [...columns];
     }
@@ -85,13 +93,15 @@ export function moveCard(
 ): Array<ColData> {
   const colIdx = columns.findIndex((col) => col.id === colId);
   if (colIdx === -1) throw new Error("Column with moving card was not found");
-  const cardIdx = columns[colIdx].cards.findIndex((card) => card.id === cardId);
+  let cards = columns[colIdx].cards;
+  if (!cards) cards = [];
+  const cardIdx = cards.findIndex((card) => card.id === cardId);
   if (cardIdx === -1) throw new Error("Moving card was not found");
-  const newCards = [...columns[colIdx].cards];
+  const newCards = [...cards];
 
   const card = { ...newCards[cardIdx] };
 
-  const newOrder = Math.max(0, Math.min(card.order - amount, newCards.length));
+  const newOrder = Math.max(1, Math.min(card.order + amount, newCards.length));
   if (newOrder === card.order) return columns;
   const replacedCardIdx = newCards.findIndex((c) => {
     return c.order == newOrder;
@@ -107,6 +117,32 @@ export function moveCard(
 
   const newCols = [...columns];
   newCols.splice(colIdx, 1, newCol);
+  return newCols;
+}
+export function moveColumn(
+  columns: Array<ColData>,
+  colId: string,
+  amount: number,
+): Array<ColData> {
+  const colIdx = columns.findIndex((col) => col.id === colId);
+  if (colIdx === -1) throw new Error("No column found, can't move nothing");
+  const col1 = columns[colIdx];
+  const newOrder = Math.max(1, Math.min(col1.order + amount, columns.length));
+  console.log(col1.order, newOrder);
+  if (newOrder === col1.order) return columns;
+  const col2Idx = columns.findIndex((col) => col1.order + amount === col.order);
+  const newCols = [...columns];
+  if (col2Idx !== -1) {
+    newCols.splice(col2Idx, 1, {
+      ...columns[col2Idx],
+      order: col1.order,
+    });
+  }
+  newCols.splice(colIdx, 1, {
+    ...col1,
+    order: col1.order + amount,
+  });
+
   return newCols;
 }
 export function swapColumns(
@@ -196,6 +232,7 @@ export function removeTag(
   cardData: CardData,
   tagId: string,
 ): CardData {
+  if (!cardData.tagIds || !kanban.tags) return cardData;
   kanban.tags = kanban.tags.filter((tag) => {
     tag.id !== tagId;
   });
