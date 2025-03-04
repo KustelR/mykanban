efc02edfd8f2e26fe7c6381fc27b8f064e74d787
KustelR <dmitriysstudios2@gmail.com>
2025-03-05 02:01:00 +0400

    sending updates to backend only if needed

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { setKanbanAction } from "@/lib/features/kanban/kanbanSlice";
+import { updateKanban } from "@/lib/features/kanban/kanbanSlice";
 import { updateLastChanged } from "@/lib/features/lastChanged/lastChangedSlice";
 import { useAppDispatch, useAppStore } from "@/lib/hooks";
 import Kanban from "@/components/kanban/Kanban";
@@ -18,10 +18,7 @@ export default function Project() {
   useEffect(() => {
     let id = searchParams.get("id");
     if (!id) return;
-    const uploadTimer = updateProject(id, store, dispatch);
-    return () => {
-      clearInterval(uploadTimer);
-    };
+    updateProject(id, store, dispatch);
   }, []);
   const data: KanbanState = store.getState().kanban;
   let id = searchParams.get("id");
@@ -32,20 +29,27 @@ export default function Project() {
   }
   return (
     <div className="">
-      <Kanban defaultLabel="TEST" className="w-full md:max-w-fit"></Kanban>
+      <Kanban className="w-full md:max-w-fit"></Kanban>
       <ManualControl></ManualControl>
     </div>
   );
 }
 
 function updateProject(id: string, store: EnhancedStore, dispatch: any) {
-  return setInterval(() => {
-    const data: KanbanState = store.getState().kanban;
+  let lastHash: string = "";
+  store.subscribe(() => {
+    const lastChanged: LastChangedState = store.getState().lastChanged;
+    if (lastHash == lastChanged.hash && lastChanged.hash != "") {
+      return;
+    }
+    lastHash = lastChanged.hash;
+    const project: KanbanState = store.getState().kanban;
+    if (project.name == "fake name fake name") return;
     const projectHost = process.env.NEXT_PUBLIC_PROJECT_HOST;
-    if (!projectHost) return;
-    axios.put(`${projectHost}/update?id=${id}`, data);
-    dispatch(updateLastChanged());
-  }, 10000);
+    if (!projectHost)
+      throw new Error("Can't get projectAPI host, can't update");
+    axios.put(`${projectHost}/update?id=${id}`, project);
+  });
 }
 
 async function createProject(data: KanbanState) {
@@ -60,9 +64,8 @@ async function readProject(id: string, dispatch: any) {
   const projectHost = process.env.NEXT_PUBLIC_PROJECT_HOST;
   if (!projectHost) return;
   const r = await axios.get(`${projectHost}/read?id=${id}`);
-  console.log(r.data);
   addProjectToStorage(id, r.data.name);
-  dispatch(setKanbanAction(r.data));
+  updateKanban(dispatch, r.data);
 }
 
 function addProjectToStorage(projectId: string, name?: string) {
