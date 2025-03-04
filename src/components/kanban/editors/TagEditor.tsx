c9d55c2e852026101c14f1a5fd9e5000df71c0c9
KustelR <dmitriysstudios2@gmail.com>
2025-03-05 02:59:47 +0400

    tag suggestions

--- src/components/kanban/editors/TagEditor.tsx
+++ src/components/kanban/editors/TagEditor.tsx
@@ -5,6 +5,9 @@ import TextInput from "@/shared/TextInput";
 import { nanoid } from "@reduxjs/toolkit";
 import React, { useState } from "react";
 import TagList from "../TagList";
+import Tag from "../Tag";
+import DeleteIcon from "@public/delete.svg";
+import ActionMenu from "@/components/ui/ActionMenu";
 
 type TagEditorProps = {
   tags: Array<TagData>;
@@ -15,45 +18,28 @@ type TagEditorProps = {
 
 export default function TagEditor(props: TagEditorProps) {
   const { tags, setTags, cardId, addTagIdToCard } = props;
-  const [tag, setTag] = useState<TagData | null>(null);
+  const [tag, setTag] = useState<TagData>(createTag());
   return (
     <section>
       <header className="font-semibold">Tag Editor</header>
-      {tagForm(
-        tag,
-        (t) => {
-          setTags([...tags, t]);
-          if (addTagIdToCard) addTagIdToCard(t.id);
-        },
-        cardId,
-      )}
-      <strong>Current tags</strong>
-      <TagList tags={[...tags, ...(tag ? [tag] : [])]}></TagList>
+      {tagForm(tag, setTag)}
+      {renderTagSuggestions(tags, tag)}
     </section>
   );
 }
 
-function tagForm(
-  tag: TagData | null,
-  setTag: (arg: TagData) => void,
-  cardId?: string,
-) {
-  const [localTag, setLocalTag] = useState(
-    tag ? tag : createTag(undefined, undefined, cardId),
-  );
+function tagForm(tag: TagData, setTag: (arg: TagData) => void) {
   return (
     <form
       className="block *:block space-y-1"
       onSubmit={(e) => {
         e.preventDefault();
-        setTag(localTag);
-        setLocalTag(createTag(undefined, undefined, cardId));
         (e.target as HTMLFormElement).reset();
       }}
     >
       <TextInput
         onChange={(e) => {
-          setLocalTag({ ...localTag, name: e.target.value });
+          setTag({ ...tag, name: e.target.value });
         }}
         placeholder={tag?.name}
         id="tag_name"
@@ -62,7 +48,7 @@ function tagForm(
       <label htmlFor="tag_color">Color</label>
       <input
         onChange={(e) => {
-          setLocalTag({ ...localTag, color: e.target.value });
+          setTag({ ...tag, color: e.target.value });
         }}
         id="tag_color"
         type="color"
@@ -75,8 +61,70 @@ function tagForm(
 function createTag(name?: string, color?: string, cardId?: string): TagData {
   return {
     id: nanoid(),
-    name: name ? name : "fake",
+    name: name ? name : "",
     color: color ? color : "#ff0000",
     cardId: cardId ? cardId : "not a card",
   };
+}
+
+function SuggestedTag(props: { data: TagData; tags: TagData[] }) {
+  const { data, tags } = props;
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      className="relative"
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
+      {isHovered && renderActionMenu(data, tags, () => {})}
+      <Tag data={data}></Tag>
+    </div>
+  );
+}
+
+function renderTagSuggestions(tags: TagData[], tag: TagData | null) {
+  return (
+    <section>
+      {" "}
+      <strong>Suggested tags</strong>
+      <ul>
+        {[...tags, ...(tag ? [tag] : [])]
+          .filter((t) => t.name.startsWith(tag?.name ? tag.name : ""))
+          .slice(0, 5)
+          .map((item, idx) => {
+            return (
+              <li key={idx}>
+                <SuggestedTag data={item} tags={tags} />
+              </li>
+            );
+          })}
+      </ul>
+    </section>
+  );
+}
+
+function renderActionMenu(
+  tag: TagData,
+  tags: Array<TagData>,
+  setTags: (arg: Array<TagData>) => void,
+) {
+  const options = [
+    {
+      icon: DeleteIcon,
+      className: "bg-red-800 hover:bg-red-900",
+      callback: () => {
+        setTags(
+          tags.filter((t) => {
+            t.id != tag.id;
+          }),
+        );
+      },
+    },
+  ];
+
+  return <ActionMenu options={options} />;
 }
