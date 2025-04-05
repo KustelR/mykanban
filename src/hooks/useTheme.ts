5bdd13b8821aa7cb9cc0f8b78bb24e1cf8120c0d
KustelR <dmitriysstudios2@gmail.com>
2025-04-05 18:18:52 +0400

    overkill class for theme state

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,12 +1,43 @@
-export function useTheme(): () => "light" | "dark" | undefined {
-  return () => {
-    try {
-      let theme = localStorage.getItem("theme");
+type ColorTheme = undefined | "dark" | "light";
 
-      if (theme !== "dark" && theme !== "light") return undefined;
-      else return theme;
-    } catch {
-      return undefined;
-    }
-  };
+class Themes {
+  static theme: ColorTheme;
+  static subscribers: Array<(theme: ColorTheme) => void> = [];
+  constructor() {
+    const filteredTheme = filterTheme(localStorage.getItem("theme"));
+    this.setTheme(filteredTheme);
+  }
+  getTheme() {
+    const theme = localStorage.getItem("theme");
+    const filteredTheme = filterTheme(theme);
+    return filteredTheme;
+  }
+  setTheme(theme: ColorTheme) {
+    Themes.theme = theme;
+    try {
+      if (theme) localStorage.setItem("theme", theme);
+    } catch (err) {}
+    Themes.subscribers.forEach((s) => s(theme));
+  }
+  subscribe(callback: (theme: ColorTheme) => void) {
+    Themes.subscribers.push(callback);
+  }
+}
+
+export function useTheme(): {
+  get: () => ColorTheme;
+  set: (theme: ColorTheme) => void;
+  subscribe: (callback: (theme: ColorTheme) => void) => void;
+} {
+  const t = new Themes();
+  return { get: t.getTheme, set: t.setTheme, subscribe: t.subscribe };
+}
+
+function filterTheme(data: any): ColorTheme {
+  const theme = localStorage.getItem("theme");
+  let filteredTheme: undefined | "dark" | "light";
+  if (theme !== "dark" && theme != "light") filteredTheme = undefined;
+  else filteredTheme = theme;
+
+  return filteredTheme;
 }
